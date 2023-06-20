54d7e598c474f156126d8561ca31ffc118cfc775
David <73008040+dcheung0720@users.noreply.github.com>
2023-06-19 22:30:06 -0500

    authentication added

--- frontend/src/components/navigation.js
+++ frontend/src/components/navigation.js
@@ -2,12 +2,33 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { signInWithGoogle, signOut, useUserState } from '../utilities/firebase';
 
 const Navigation = () =>{
+  const [user] = useUserState();
+    const SignInButton = () => (
+      <button className="btn btn-secondary btn-sm"
+          onClick={() => signInWithGoogle()}>
+        Sign In
+      </button>
+    );
+
+    const SignOutButton = () => (
+      <>
+        <button className="btn btn-secondary btn-sm"
+          onClick={() => signOut()}>
+          Sign Out
+        </button>
+      </>
+
+    );
+
+    console.log(user);
 
     return ( <Navbar bg="dark" data-bs-theme="dark">
       <Container style = {{justifyContent: 'center'}}>
         <Navbar.Brand href="#home" style = {{color: 'white', fontSize: "30px"}}>Turbo Math</Navbar.Brand>
+        { user ? <SignOutButton /> : <SignInButton /> }
       </Container>
     </Navbar>)
 
---
+++ frontend/src/utilities/firebase.js
@@ -0,0 +1,30 @@
+import { getAuth, GoogleAuthProvider, onIdTokenChanged, signInWithPopup, signOut} from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, onValue, ref, set } from 'firebase/database';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB9CxFVb8iKl04zDNPtngcmd4ygvok4YG8",
+    authDomain: "turbomath-a0c94.firebaseapp.com",
+    databaseURL: "https://turbomath-a0c94-default-rtdb.firebaseio.com",
+    projectId: "turbomath-a0c94",
+    storageBucket: "turbomath-a0c94.appspot.com",
+    messagingSenderId: "287662292669",
+    appId: "1:287662292669:web:0ae78b89346f9d59b941d4",
+    measurementId: "G-HGSPEXVBLN"
+  };
+
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+
+
+
+export const signInWithGoogle = () => {
+    signInWithPopup(getAuth(firebase), new GoogleAuthProvider());
+  };
+
+const firebaseSignOut = () => signOut(getAuth(firebase));
+
+export { firebaseSignOut as signOut };
+
+export const useUserState = () => useAuthState(getAuth(firebase));
