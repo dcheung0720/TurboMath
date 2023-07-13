e33aa6c270a260e0561aa05025c2ee2c2aaec2d2
David <davidcheung231@gmail.com>
2023-07-13 02:57:59 -0500

    added font awesome to leaderboard

--- frontend/src/components/LeaderBoard.js
+++ frontend/src/components/LeaderBoard.js
@@ -1,4 +1,6 @@
 import FlipMove from 'react-flip-move';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrophy } from '@fortawesome/free-solid-svg-icons';
 
 const LeaderBoard = ({ room }) => {
   return (
@@ -15,9 +17,14 @@ const LeaderBoard = ({ room }) => {
             <FlipMove typeName={null}>
               {Object.entries(room.Players)
                 .sort((a, b) => b[1].score - a[1].score)
-                .map((player, index) => (                  
+                .map((player, index) => ( 
+                    // key required for react to differentiate items in a list                 
                     <tr key={player[0]}>
-                      <th scope="row">{index + 1}</th>
+                      <th scope="row">{index + 1} &nbsp;
+                      {/* gold silver and bronze trophies */}
+                      {index == 0 ?<FontAwesomeIcon icon={faTrophy} style={{color: "#fdec08",}} /> :
+                       index == 1? <FontAwesomeIcon icon={faTrophy} style={{color: "#C0C0C0",}} /> :
+                       index == 2? <FontAwesomeIcon icon={faTrophy} style={{color: "#CD7F32",}}/>: <></>}</th>
                       <td>{player[1].name}</td>
                       <td>{player[1].score}</td>
                     </tr>
