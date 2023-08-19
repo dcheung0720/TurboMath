d2b9081a121a209c38c2361054c1b5d3ac320685
David <davidcheung231@gmail.com>
2023-08-19 17:13:27 -0500

    added basic content to modal

--- frontend/src/components/CreateModal.js
+++ frontend/src/components/CreateModal.js
@@ -1,14 +1,29 @@
 import "./CreateModal.css"
+import { Button } from "react-bootstrap";
+import { Modal } from "react-bootstrap";
 
 const CreateModal = ({ChangeModal}) =>{
-    return(
-        <div className = "modalBackground" onClick={() => ChangeModal()}>
-            <div className = "modalContainer">
-                This is the modal!
-            </div>
+    return (
+        <div
+          className="modalBackground" onClick={() => ChangeModal()}
+          style={{ display: 'block'}}
+        >
+          <Modal.Dialog>
+            <Modal.Header closeButton>
+              <Modal.Title>Modal title</Modal.Title>
+            </Modal.Header>
+    
+            <Modal.Body>
+              <p>Modal body text goes here.</p>
+            </Modal.Body>
+    
+            <Modal.Footer>
+              <Button variant="secondary">Close</Button>
+              <Button variant="primary">Save changes</Button>
+            </Modal.Footer>
+          </Modal.Dialog>
         </div>
-    )    
-
+      );  
 }
 
 export default CreateModal;
