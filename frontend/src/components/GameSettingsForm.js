aad8a24880cb1c17b8d80603e39dfe4f5d1bc863
David <davidcheung231@gmail.com>
2023-08-20 16:17:14 -0500

    form data created

--- frontend/src/components/GameSettingsForm.js
+++ frontend/src/components/GameSettingsForm.js
@@ -2,6 +2,7 @@ import Form from 'react-bootstrap/Form';
 import "./GameSettingsForm.css"
 import { Button } from "react-bootstrap";
 import { useState } from 'react';
+import { setData, useData, useUserState } from '../utilities/firebase';
 
 
 const GameSettingsForm = ({GameMode}) =>{
@@ -11,13 +12,52 @@ const GameSettingsForm = ({GameMode}) =>{
     const [number1, setNumber1] = useState(1);
     const [number2, setNumber2] = useState(1);
 
-    const CreateRoom = () =>{
+    // gameroomData
+    const [data, error] = useData('GameRooms');
+    const [user] = useUserState();
+    let roomIDs;
+    
+    if (data){
+        roomIDs = Object.keys(data);
+    }
 
+    const CreateRoom = (e) =>{
+        e.preventDefault();
+        const id = FindId();
+        const object = {
+            "Mode" : mode,
+            "Players": user.uid,
+            "Difficulty1" : number1,
+            "Difficulty2" : number2,
+            "Problems" : {
+                "number1": GenerateNumbers(number1),
+                "number2": GenerateNumbers(number2)
+            } 
+        }
+        console.log(object);
+    }
+
+    const GenerateNumbers = (numDigits) =>{
+        console.log(numDigits)
+        switch(numDigits){
+            case "1":
+                return Math.floor(Math.random() * 9 + 1);
+            case "2":
+                return Math.floor(Math.random() * 90 + 10);   
+            case "3":        
+                return Math.floor(Math.random() * 900 + 100);       
+        }
     };
 
-    console.log(mode);
-    console.log(number1);
-    console.log(number2)
+    const FindId = () =>{
+        let id = Math.floor(Math.random() * 10000);
+        // randomize until there isn't a match
+        while(roomIDs.includes(id)){
+            id = Math.floor(Math.random() * 10000);
+        }
+
+        return id;
+    }
 
     const handleStateChange = (e, setState) =>{
         setState(e.target.value);
@@ -57,7 +97,7 @@ const GameSettingsForm = ({GameMode}) =>{
                 <span>Digits</span>          
             </Form.Group>
 
-            <Button variant="btn btn-success" type = "submit">Create</Button>
+            <Button variant="btn btn-success" type = "submit" onClick = {(e)=>CreateRoom(e)} >Create</Button>
           </fieldset>
         </Form>
     )
