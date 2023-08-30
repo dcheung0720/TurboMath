eab1ba2bafd960491b5525eb1f2a00670c112af4
David <73008040+dcheung0720@users.noreply.github.com>
2023-08-30 15:53:19 -0500

    dumb way to filter images

--- frontend/src/utilities/firebase.js
+++ frontend/src/utilities/firebase.js
@@ -70,7 +70,7 @@ export const useImages = (path) =>{
     listAll(sRef(storage, path)).then((res)=>{
         res.items.forEach(x => {
           getDownloadURL(x).then(url =>{
-            setImageLists((prev) => prev.length < 4? [...prev, url] : [...prev]);
+            setImageLists((prev) => !prev.includes(url)? [...prev, url] : [...prev]);
           })
         })
     })
