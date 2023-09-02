6a696f17c3e715bcb5db8770f657927211a849c0
David <73008040+dcheung0720@users.noreply.github.com>
2023-09-02 13:48:48 -0500

    added flame icon

--- frontend/src/components/ProfileStats.js
+++ frontend/src/components/ProfileStats.js
@@ -6,7 +6,7 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrophy } from '@fortawesome/free-solid-svg-icons';
+import { faTrophy, faFire } from '@fortawesome/free-solid-svg-icons';
 
 const ProfileStats = () =>{
 
@@ -28,9 +28,9 @@ const ProfileStats = () =>{
         <Card.Body >
             <div className = "header" style = {{display: "flex", justifyContent:"space-between", alignItems: "center" }}>
                 <div style={{ flex: 1, textAlign: "center" }}>
-                    <Card.Title style={{marginBottom: "5px"}}>Personal High Scores</Card.Title>
+                    <Card.Title style={{marginBottom: "5px"}}>Personal High Scores <FontAwesomeIcon icon={faFire} style={{color: "#ff0000"}} /></Card.Title>
                 </div>
-                <span className = "selection" style = {{display: "flex", alignItems: "center", position: "absolute", right: "3px", top: "3px", zIndex: "1000" }}>
+                <span className = "selection" style = {{display: "flex", alignItems: "center", position: "absolute", right: "3px", top: "3px", zIndex: "60" }}>
                     <div>Difficulty: </div>
                     <DropdownButton  key="secondary" 
                     id="dropdown-variants-Secondary" 
@@ -38,7 +38,6 @@ const ProfileStats = () =>{
                     style ={{margin:"5px"}}
                     onSelect = {(eKey) => handleSelectDifficulty(eKey)}
                     >
-
                         <Dropdown.Item href="#/action-1" eventKey="1x1">1x1</Dropdown.Item>
                         <Dropdown.Item href="#/action-2" eventKey="2x1">2x1</Dropdown.Item>
                         <Dropdown.Item href="#/action-3" eventKey="3x1">3x1</Dropdown.Item>
